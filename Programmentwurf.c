8598f5997112bf164dd4c18239b040e792910c43
Fabian Klotz <fabian.klotz2003@gmail.com>
2023-03-17 15:42:44 +0100

    update Programmentwurf
    functional

--- Programmentwurf.c
+++ Programmentwurf.c
@@ -6,25 +6,25 @@
 #include <time.h>
 
 // cycletimes in ms (1s = 1000ms)
-#define CYCLE1 1000
-#define CYCLE2 5000
-#define CYCLE3 10000
-#define CYCLE4 100000
 
-#define NUM_THREADS 4
 #define NUM_TASKS 4
+#define NUM_FUNC 5
+
+//array für cycletimes
+int cycleTimes[NUM_TASKS] = {1000, 5000, 10000, 100000};
 
 double a = 5;
 double b = 8;
 
 struct timeval start_time, current_time;
 
-double array[NUM_TASKS][NUM_THREADS] = {
+double array[NUM_FUNC][NUM_TASKS] = {
     //          T1  T2  T3  T4
     /*add*/    {3,  0,  0,  0},
     /*multi*/  {2,  2,  0,  0},
     /*div*/    {1,  1,  1,  0},
-    /*mod*/    {0,  0,  0,  1}
+    /*mod*/    {0,  0,  0,  1},
+    /*div2*/   {0,  3,  0,  0}
 };
 
 void wait_200ms() {
@@ -39,12 +39,12 @@ void registration(int thread, int priority, int task){
         array[task-1][thread-1] = priority;
 }
 
-double addition(double a, double b, int firstRun)
+double addition(double a, double b, double firstRun)
 {
     int func = 1;
     if(firstRun == 1){
         //hier kann eine registrierung der Funktion stattfinden
-        registration(3, 2, func); //Thread 3 registriert Task 1 mit Priorität 2
+        //registration(3, 2, func); //Thread 3 registriert Task 1 mit Priorität 2
     }
     else{
     double result;
@@ -54,7 +54,7 @@ double addition(double a, double b, int firstRun)
 }
 }
 
-double multiplication(double a, double b, int firstRun)
+double multiplication(double a, double b, double firstRun)
 {
     int func = 2;
     if(firstRun == 1){
@@ -68,7 +68,7 @@ double multiplication(double a, double b, int firstRun)
 }
 }
 
-double division(double a, double b, int firstRun)
+double division(double a, double b, double firstRun)
 {
     int func = 3;
     if(firstRun == 1){
@@ -76,6 +76,10 @@ double division(double a, double b, int firstRun)
         //registration(4, 3, func);
     }
     else{
+        if(b == 0){
+            printf("Division durch 0 nicht möglich");
+            return 0;
+        }
     double result;
     result = a / b;
     printf("%lf", result);
@@ -83,7 +87,7 @@ double division(double a, double b, int firstRun)
 }
 }
 
-double modulo(double a, double b, int firstRun)
+double modulo(double a, double b, double firstRun)
 {
     int func = 4;
     if(firstRun == 1){
@@ -98,39 +102,44 @@ double modulo(double a, double b, int firstRun)
 }
 }
 
-void checkForTasks(int thread, double array[][NUM_THREADS]) {
-    for(int j = 0; j < NUM_TASKS; j++){
+double division2(double a, double b, double firstRun)
+{
+    int func = 5;
+    if(firstRun == 1){
+        //hier kann eine registrierung der Funktion stattfinden
+        //registration(2, 3, func);
+    }
+    else{
+        if(a == 0){
+            printf("Division durch 0 nicht möglich");
+            return 0;
+        }
+    double result;
+    result = b / a;
+    printf("%lf", result);
+    return result;
+}
+}
+
+double (*fktPointer[NUM_FUNC])(double, double, double) = {addition, multiplication, division, modulo, division2};
+
+void checkForTasks(int thread, double array[][NUM_TASKS]) {
+    for(int j = 0; j < NUM_FUNC; j++){
         double *p = &array[0][thread-1];
         int i;
-        for (i = 0; i < NUM_TASKS; i++) {
+        for (i = 0; i < NUM_FUNC; i++) {
             if (*p == j+1) {
-                switch(i%NUM_TASKS){
-                    case 0:
-                        addition(a, b, 0);
-                        printf("\n");
-                        break;
-                    case 1:
-                        multiplication(a, b, 0);
-                        printf("\n");
-                        break;
-                    case 2:
-                        division(a, b, 0);
-                        printf("\n");
-                        break;
-                    case 3:
-                        modulo(a, b, 0);
-                        printf("\n");
-                        break;
-                };  
+                fktPointer[i%NUM_FUNC](a, b, 0);
+                printf("\n");
             }
-            p = p + NUM_THREADS;
+            p = p + NUM_TASKS;
         }
     }
     
         
 }
 
-void *primary_task()
+void *first_task()
 {
     struct timeval start_time, current_time;
     int elapsed_time_ms = 0;
@@ -138,22 +147,22 @@ void *primary_task()
     while (1)
     {
         gettimeofday(&start_time, NULL);
-        printf("Primary task running\n");
+        printf("\nPrimary task running\n\n");
         fflush(stdout);
         checkForTasks(1, array);
         do
         {
             gettimeofday(&current_time, NULL);
             elapsed_time_ms = (current_time.tv_sec - start_time.tv_sec) * 1000 + (current_time.tv_usec - start_time.tv_usec) / 1000;
-        }while (elapsed_time_ms < CYCLE1);
+        }while (elapsed_time_ms < cycleTimes[0]);
 
-        printf("Primary task finished in %d ms\n", elapsed_time_ms);
+        printf("\nPrimary task finished in %d ms\n", elapsed_time_ms);
         fflush(stdout);
         elapsed_time_ms = 0;
     }
 }
 
-void *secondary_task()
+void *second_task()
 {
     struct timeval start_time, current_time;
     int elapsed_time_ms = 0;
@@ -161,16 +170,16 @@ void *secondary_task()
     while (1)
     {
         gettimeofday(&start_time, NULL);
-        printf("Secondary task running\n");
+        printf("\nSecondary task running\n\n");
         fflush(stdout);
         checkForTasks(2, array);
         do
         {
             gettimeofday(&current_time, NULL);
             elapsed_time_ms = (current_time.tv_sec - start_time.tv_sec) * 1000 + (current_time.tv_usec - start_time.tv_usec) / 1000;
-        } while (elapsed_time_ms < CYCLE2);
+        } while (elapsed_time_ms < cycleTimes[1]);
 
-        printf("Secondary task finished in %d ms\n", elapsed_time_ms);
+        printf("\nSecondary task finished in %d ms\n", elapsed_time_ms);
         fflush(stdout);
         elapsed_time_ms = 0;
     }
@@ -184,16 +193,16 @@ void *third_task()
     while (1)
     {
         gettimeofday(&start_time, NULL);
-        printf("Third task running\n");
+        printf("\nThird task running\n\n");
         fflush(stdout);
         checkForTasks(3, array);
         do
         {
             gettimeofday(&current_time, NULL);
             elapsed_time_ms = (current_time.tv_sec - start_time.tv_sec) * 1000 + (current_time.tv_usec - start_time.tv_usec) / 1000;
-        } while (elapsed_time_ms < CYCLE3);
+        } while (elapsed_time_ms < cycleTimes[2]);
 
-        printf("Third task finished in %d ms\n", elapsed_time_ms);
+        printf("\nThird task finished in %d ms\n", elapsed_time_ms);
         fflush(stdout);
         elapsed_time_ms = 0;
     }
@@ -207,57 +216,49 @@ void *fourth_task()
     while (1)
     {
         gettimeofday(&start_time, NULL);
-        printf("Fourth task running\n");
+        printf("\nFourth task running\n\n");
         fflush(stdout);
         checkForTasks(4, array);
         do{
             gettimeofday(&current_time, NULL);
             elapsed_time_ms = (current_time.tv_sec - start_time.tv_sec) * 1000 + (current_time.tv_usec - start_time.tv_usec) / 1000;
-        }while (elapsed_time_ms < CYCLE4);
+        }while (elapsed_time_ms < cycleTimes[3]);
 
-        printf("Fourth task finished in %d ms\n", elapsed_time_ms);
+        printf("\nFourth task finished in %d ms\n", elapsed_time_ms);
         fflush(stdout);
         elapsed_time_ms = 0;
     }
 }
 
-void printArray(double array[][NUM_THREADS]) {
+void printArray(double array[][NUM_TASKS])
+{
     int i, j;
     printf("Konfiguration:\n\n");
-    printf("Task       1| 2| 3| 4| \n");
-    printf("-----------------------\n");
-    for (i = 0; i < NUM_TASKS; i++) {
-        printf("Func %d:    ", i+1);
-            for (j = 0; j < NUM_THREADS; j++) {
-                printf("%d  ", (int) array[i][j]);
-            }
-        printf("\n-----------------------\n");
+    printf("+----------+-----------+-----------+-----------+-----------+\n");
+    printf("| Funktion | Aufgabe 1 | Aufgabe 2 | Aufgabe 3 | Aufgabe 4 |\n");
+    printf("+----------+-----------+-----------+-----------+-----------+\n");
+    for (i = 0; i < NUM_FUNC; i++)
+    {
+        printf("| %8d |", i + 1);
+        for (j = 0; j < NUM_TASKS; j++)
+        {
+            printf(" %9d |", (int)array[i][j]);
+        }
+        printf("\n+----------+-----------+-----------+-----------+-----------+\n");
     }
-    printf("\n");
 }
 
-void init()
-{
- 
-    pthread_t thread_id1, thread_id2, thread_id3, thread_id4;
-    // Start primary task
-    pthread_create(&thread_id1, NULL, primary_task, NULL);
-    wait_200ms();
-    // Start secondary task
-    pthread_create(&thread_id2, NULL, secondary_task, NULL);
-    wait_200ms();
-    // Start third task
-    pthread_create(&thread_id3, NULL, third_task, NULL);
-    wait_200ms();
-    // Start fourth task
-    pthread_create(&thread_id4, NULL, fourth_task, NULL);
-
-
-    pthread_join(thread_id1, NULL);
-    pthread_join(thread_id2, NULL);
-    pthread_join(thread_id3, NULL);
-    pthread_join(thread_id4, NULL);
+void init(int num_threads, void* (*task_functions[])(void*)) {
+    pthread_t threads[num_threads];
+    for (int i = 0; i < num_threads; i++) {
+        pthread_create(&threads[i], NULL, task_functions[i], NULL);
+        wait_200ms();
+    }
+    for (int i = 0; i < num_threads; i++) {
+        pthread_join(threads[i], NULL);
+    }
 }
+
 
 int main()
 {
@@ -266,7 +267,11 @@ int main()
     division(a, b, 1);
     modulo(a, b, 1);
 
-    
     printArray(array);
-    init();
+
+    void* (*tasks[])(void*) = { first_task, second_task, third_task, fourth_task };
+    init(4, tasks);
+
+    init(NUM_TASKS, tasks);
+    return 0;
 }
