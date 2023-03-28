153968457ec1fb9df56b44c4dee1634e10603bcf
Fabian Klotz <fabian.klotz2003@gmail.com>
2023-03-28 13:40:03 +0200

    update

--- test2.c
+++ test2.c
@@ -3,17 +3,20 @@
 #include <pthread.h>
 #include <unistd.h>
 #include <sys/time.h>
+#include <signal.h>
 
 #define MAX_TASKS 4
 
-typedef struct node {
+typedef struct node
+{
     void (*func)();
     int priority;
     struct node *next;
 } Node;
 
-typedef struct {
-    int frequency; // in Sekunden
+typedef struct
+{
+    int frequency;   // in Sekunden
     Node *functions; // verkettete Liste von Funktionen
 } Task;
 
@@ -21,10 +24,10 @@ Task tasks[MAX_TASKS] = {
     {.frequency = 1, .functions = NULL},
     {.frequency = 5, .functions = NULL},
     {.frequency = 10, .functions = NULL},
-    {.frequency = 100, .functions = NULL}
-};
+    {.frequency = 100, .functions = NULL}};
 
-void add_function(Task *task, void (*function)(), int priority) {
+void add_function(Task *task, void (*function)(), int priority)
+{
     Node *new_node = (Node *)malloc(sizeof(Node));
     new_node->func = function;
     new_node->priority = priority;
@@ -32,14 +35,19 @@ void add_function(Task *task, void (*function)(), int priority) {
     task->functions = new_node;
 }
 
-void insert_sorted(Node **head_ref, Node *new_node) {
+void insert_sorted(Node **head_ref, Node *new_node)
+{
     Node *curr;
-    if (*head_ref == NULL || (*head_ref)->priority <= new_node->priority) {
+    if (*head_ref == NULL || (*head_ref)->priority <= new_node->priority)
+    {
         new_node->next = *head_ref;
         *head_ref = new_node;
-    } else {
+    }
+    else
+    {
         curr = *head_ref;
-        while (curr->next != NULL && curr->next->priority > new_node->priority) {
+        while (curr->next != NULL && curr->next->priority > new_node->priority)
+        {
             curr = curr->next;
         }
         new_node->next = curr->next;
@@ -47,17 +55,22 @@ void insert_sorted(Node **head_ref, Node *new_node) {
     }
 }
 
-void sort_functions(Task *task) {
+void sort_functions(Task *task)
+{
     Node *curr = task->functions;
     task->functions = NULL;
-    while (curr != NULL) {
+    while (curr != NULL)
+    {
         Node *next = curr->next;
         insert_sorted(&task->functions, curr);
         curr = next;
     }
 }
 
-void *task_thread(void *arg) {
+pthread_mutex_t mutex;
+
+void *task_thread(void *arg)
+{
     Task *task = (Task *)arg;
     struct timespec sleep_time, remaining_time;
     long long elapsed_time_ns;
@@ -66,72 +79,109 @@ void *task_thread(void *arg) {
     struct timespec last_start_time;
     clock_gettime(CLOCK_REALTIME, &last_start_time);
 
-    while (1) {
-        struct timeval start_time, end_time;
-        gettimeofday(&start_time, NULL);
+    // Mutex initialisieren
+    if (pthread_mutex_init(&mutex, NULL) != 0)
+    {
+        printf("Mutex-Initialisierung fehlgeschlagen\n");
+        exit(1);
+    }
 
+    while (1)
+    {
         sort_functions(task);
         Node *curr = task->functions;
-        printf("Task %ds\n", task->frequency);
+        printf("\033[32mTask %ds\033[0m\n", task->frequency);
 
-        while (curr != NULL) {
-            struct timeval func_start_time, func_end_time;
-            gettimeofday(&func_start_time, NULL);
+        while (curr != NULL)
+        {
+            struct timespec func_start_time, func_end_time;
+            clock_gettime(CLOCK_REALTIME, &func_start_time);
+
+            // Mutex sperren, bevor der kritische Abschnitt ausgeführt wird
+            pthread_mutex_lock(&mutex);
             curr->func();
-            gettimeofday(&func_end_time, NULL);
+            // Mutex entsperren, nachdem der kritische Abschnitt ausgeführt wurde
+            pthread_mutex_unlock(&mutex);
 
-            elapsed_time_ns = (func_end_time.tv_sec - func_start_time.tv_sec) * 1000000000LL +
-                (func_end_time.tv_usec - func_start_time.tv_usec) * 1000LL;
+            clock_gettime(CLOCK_REALTIME, &func_end_time);
 
-            // verzögere die Ausführung der nächsten Funktion um die verbleibende Zeit
-            sleep_time.tv_sec = (task->frequency - elapsed_time_ns / 1000000000) - 1;
-            sleep_time.tv_nsec = 1000000000 - (elapsed_time_ns % 1000000000);
-
-            // Wenn die verbleibende Zeit negativ ist, wird die Funktion sofort ausgeführt
-            if (sleep_time.tv_nsec < 0) {
-                sleep_time.tv_sec = 0;
-                sleep_time.tv_nsec = 0;
-            }
-
-            
-
+            long long elapsed_time_ms = (func_end_time.tv_sec - func_start_time.tv_sec) * 1000LL +
+                                        (func_end_time.tv_nsec - func_start_time.tv_nsec) / 1000000LL;
+            sleep_time.tv_sec = (task->frequency - elapsed_time_ms / 1000L) - 1;
             curr = curr->next;
         }
-
         printf("\n");
-        nanosleep(&sleep_time, &remaining_time);
-        gettimeofday(&end_time, NULL);
-        long elapsed_time = (end_time.tv_sec - start_time.tv_sec) * 1000L + (end_time.tv_usec - start_time.tv_usec) / 1000L;
-        printf("Elapsed time: %ld ms\n", elapsed_time);
 
         // speichere die Startzeit des aktuellen Zyklus
+        struct timespec current_time;
+        clock_gettime(CLOCK_REALTIME, &current_time);
+        long long elapsed_time_ms = (current_time.tv_sec - last_start_time.tv_sec) * 1000LL +
+                                    (current_time.tv_nsec - last_start_time.tv_nsec) / 1000000LL;
         last_start_time.tv_sec += task->frequency;
-       // clock_nanosleep(CLOCK_REALTIME, TIMER_ABSTIME, &last_start_time, NULL);
 
+        sleep_time.tv_nsec = (1000L - (elapsed_time_ms % 1000L)) * 1000000L;
+        long long remaining_time_ms = remaining_time.tv_sec * 1000LL + remaining_time.tv_nsec / 1000000LL;
+        clock_nanosleep(CLOCK_REALTIME, TIMER_ABSTIME, &last_start_time, &remaining_time);
     }
 }
 
+void selfRegister(void (*function)(), int priority, int task)
+{
+    add_function(&tasks[task], function, priority);
 
+}
 
-void func1() {
-   // add_function(&tasks[0], func1, 2);
+void func1()
+{
     printf("Function 1\n");
 }
 
-void func2() {
+void func2()
+{
     printf("Function 2\n");
 }
 
-void func3() {
+void func3()
+{
     printf("Function 3\n");
 }
 
-void func4() {
+void func4()
+{
     printf("Function 4\n");
 }
 
-int main() {
+void cleanup()
+{
+    // Mutex zerstören
+    pthread_mutex_destroy(&mutex);
+
+    // Speicher für die verketteten Listen freigeben
+    for (int i = 0; i < MAX_TASKS; i++)
+    {
+        Node *curr = tasks[i].functions;
+        while (curr != NULL)
+        {
+            Node *next = curr->next;
+            free(curr);
+            curr = next;
+        }
+    }
+}
+
+void exit_handler(int sig)
+{
+    printf("Exiting...\n");
+    cleanup();
+    exit(0);
+}
+
+int main()
+{
     // Funktionen hinzufügen
+
+    signal(SIGINT, exit_handler);
+
     add_function(&tasks[0], func1, 2);
     add_function(&tasks[0], func2, 1);
 
@@ -147,15 +197,16 @@ int main() {
     add_function(&tasks[3], func2, 2);
     add_function(&tasks[3], func3, 1);
 
-
     // Threads für jede Aufgabe erstellen
     pthread_t threads[MAX_TASKS];
-    for (int i = 0; i < MAX_TASKS; i++) {
+    for (int i = 0; i < MAX_TASKS; i++)
+    {
         pthread_create(&threads[i], NULL, task_thread, (void *)&tasks[i]);
     }
 
     // Warten, bis der Benutzer den Prozess beendet
-    while (1) {
+    while (1)
+    {
         sleep(1);
     }
 
